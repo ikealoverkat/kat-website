e70b9340816ba329f724ae198b81e887e69c4e6a
kat <katwang7749@gmail.com>
2026-03-07 12:36:36 -0500

    it is broken but if anything goes worse i will hard revert

---
+++ src/Blog.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+function Blog() {
+    return (
+        <div>
+            <div className="m-[4vh]">
+                <h1 className="text-7xl bad-handwriting">kat journal</h1>
+                <h2 className="text-5xl bad-handwriting">I write stuff sometimes</h2>
+            </div>
+        </div>
+    )   
+}
+
+export default Blog
