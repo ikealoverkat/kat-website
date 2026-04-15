47917a222b56cbadf70d0680c9762fcd08a87903
kat <katwang7749@gmail.com>
2026-04-14 23:29:48 -0400

    fix (maybe) for links not working

--- src/Empty.jsx
+++ src/Empty.jsx
@@ -10,7 +10,7 @@ function Empty() {
 function EmptyBlog() {
     return (
         <div className="m-[4vh]">
-            <a className="text-3xl font-bad-handwriting text-gray-400" href="/blog">back</a>
+            <a className="text-3xl font-bad-handwriting text-gray-400" href="./blog">back</a>
             <h1 className="font-bad-handwriting text-6xl">nothing here yet!</h1>
             <h2 className="font-bad-handwriting text-4xl text-pink-900">maybe this is a blog I haven't written yet or some information I haven't found. Check back later!</h2>
         </div>
